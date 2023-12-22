598d309ad6ebd8f9d75b7ce7d6f97989c9c7233c
shubham3398 <117896201+shubham3398@users.noreply.github.com>
2023-12-22 16:21:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	const firstName = document.getElementById("first-name");
+	const lastName = document.getElementById("last-name");
 
+	let ans = firstName.value + " " + lastName.value;
+	alert(ans);
 }
